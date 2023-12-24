8262061bc05de9327d74fc11b718056001d28fc2
Epicskylegend <adeba003@gmail.com>
2023-12-24 17:16:28 -0600

    Added collision effects to stop ship from going off screen.

--- Assets/Scripts/Player Scripts/Player Movement/Movement.cs
+++ Assets/Scripts/Player Scripts/Player Movement/Movement.cs
@@ -10,6 +10,11 @@ public class Movement : MonoBehaviour
     float moveUpSpeed = 4.5f;
     float moveDownSpeed = 5f;
 
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
+
     void Start()
     {
         
@@ -23,27 +28,28 @@ public class Movement : MonoBehaviour
 
 
 
-
+    
     void PlayerMovement()
     {
-        if(Input.GetKey(KeyCode.W))
+        
+        if (Input.GetKey(KeyCode.W) && transform.position.y < 8.961)
         {
             transform.Translate(Vector2.up * Time.deltaTime * Input.GetAxis("Vertical") * moveUpSpeed);
 
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -18.78)
         {
             transform.Translate(Vector2.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveLeftSpeed);
 
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -8.961)
         {
             transform.Translate(Vector2.up * Time.deltaTime * Input.GetAxis("Vertical") * moveDownSpeed);
 
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 18.78)
         {
             transform.Translate(Vector2.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveRightSpeed);
 
