f19ac70b5ffbeb2597067687eee6606019ba964d
Epicskylegend <adeba003@gmail.com>
2024-01-10 13:54:47 -0600

    The intervals at which asteroids spawn decreases over time.

--- Assets/Scripts/Enemy Scripts/AsteroidSpawner/AsteroidSpawner.cs
+++ Assets/Scripts/Enemy Scripts/AsteroidSpawner/AsteroidSpawner.cs
@@ -7,25 +7,44 @@ public class AsteroidSpawner : Asteroid1
     public List<Enemy> asteroidPrefabs;
     public float minY;
     public float maxY;
-    // Start is called before the first frame update
+
+    public float initialAsteroidSpawnInterval = 3.0f; // Initial spawn interval
+    public float asteroidSpawnIntervalDecrease = 0.001f; // Rate at which spawn interval decreases
+    private float lastAsteroidSpawnTime;
+
     void Start()
     {
-        InvokeRepeating("launchAsteroid", 1, 1);
-       
-       
+        lastAsteroidSpawnTime = Time.time + initialAsteroidSpawnInterval;
+        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-
-    void launchAsteroid()
+    void LaunchAsteroid()
     {
         float randomSpawn = Random.Range(minY, maxY);
         Vector2 spawnPosition = new Vector2(spawnPoint.position.x, randomSpawn);
         Asteroid1.spawnAsteroid(spawnPosition, asteroidPrefabs[0].asteroidPrefab, asteroidPrefabs[0].speed, asteroidPrefabs[0].rotationSpeed);
+    }
 
+    void Update()
+    {
+        LaunchAsteroidIntervals();
+    }
+
+    void LaunchAsteroidIntervals()
+    {
+        if (Time.time >= lastAsteroidSpawnTime)
+        {
+            LaunchAsteroid();
+            lastAsteroidSpawnTime += initialAsteroidSpawnInterval; // Add initial interval first
+
+            // Decrease spawn interval over time
+            initialAsteroidSpawnInterval -= asteroidSpawnIntervalDecrease;
+
+            // Ensure the spawn interval does not go below a certain threshold
+            if (initialAsteroidSpawnInterval < 0.2f)
+            {
+                initialAsteroidSpawnInterval = 0.2f;
+            }
+        }
     }
 }
