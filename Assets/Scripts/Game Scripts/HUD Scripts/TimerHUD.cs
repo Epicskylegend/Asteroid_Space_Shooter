9b19c218b96b6299c9447d76f281836f3b72b7c4
Epicskylegend <adeba003@gmail.com>
2024-01-10 11:10:28 -0600

    Now properly displays game time uisng canvas.

--- Assets/Scripts/Game Scripts/HUD Scripts/TimerHUD.cs
+++ Assets/Scripts/Game Scripts/HUD Scripts/TimerHUD.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TimerHUD : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text timerText;
     void Start()
     {
         
@@ -13,6 +14,6 @@ public class TimerHUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timerText.SetText("Time: " + Mathf.Round(Time.time));
     }
 }
